70175bb1862ee86b6e1d007f7dbfc8ffc1b8c87f
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-08-10 18:48:20 +0530

    <script> warning

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
+import Warning from "./Warning";
 
 const Textarea = () => {
-  return (
-    <textarea placeholder="Enter your text " spellCheck="false" onChange={(e)=>{
-        console.log(e.target.value)
-    }} className='textarea'/>
-  )
-}
+  const [text, setText] = useState("");
+  const [warning,setWarning] = useState(false);
 
-export default Textarea
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      alert("No script tag allowed");
+      newText = newText.replace("<script>", "");
+      setWarning(true);
+    }else{
+      setWarning(false);
+    }
+    setText(newText);
+   
+  };
+
+  return (
+    <div className="textarea">
+      <textarea
+        placeholder="Enter your text "
+        spellCheck="false"
+        value={text}
+        onChange={handleChange}
+        className="textarea"
+      />
+      {warning && <Warning/>}
+    </div>
+  );
+};
+
+export default Textarea;
---
+++ src/Warning.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Warning = () => {
+  return (
+    <p className='warning'>No script tag allowed!</p>
+  )
+}
+
+export default Warning
