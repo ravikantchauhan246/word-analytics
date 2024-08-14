aeea495bc25e647caec5f5592a96d36a4eb3f866
ravikantchauhan246 <ravikantchauhan246@gmail.com>
2024-08-14 15:33:25 +0530

    add others stats

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
 
 const Stats = ({text}) => {
-
+  //Characters
   const numberOfCharacters = text.length;
+
+  //Words
+  const numberOfWords = text.split(/\s/).filter((word)=>word !=="").length
+
+  // Instagram 
+
+  const instagramCharactersLeft = 280 - text.length
+
+  //Facebook 
+
+  const facebookCharactersLeft = 2200 - text.length
 
   return (
     <div className='stats' >
-      <Stat number={0} label="Words" />
+      <Stat number={numberOfWords} label="Words" />
       <Stat number={numberOfCharacters} label="Characters"/>
-      <Stat number={280} label="Instagram"/>
-      <Stat number={2200} label="Facebook"/>
+      <Stat number={instagramCharactersLeft <=280 ? instagramCharactersLeft : 0} label="Instagram"/>
+      <Stat number={facebookCharactersLeft} label="Facebook"/>
       
     </div>
   )
